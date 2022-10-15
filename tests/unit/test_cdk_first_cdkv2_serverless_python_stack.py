ab2a5c499b8f360de7f2ef4a11944336b2943556
Oscar Pérez <oscar@steadyrent.com>
2022-10-14 19:50:16 -0500

    Initial commit

---
+++ tests/unit/test_cdk_first_cdkv2_serverless_python_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from cdk_first_cdkv2_serverless_python.cdk_first_cdkv2_serverless_python_stack import CdkFirstCdkv2ServerlessPythonStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in cdk_first_cdkv2_serverless_python/cdk_first_cdkv2_serverless_python_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = CdkFirstCdkv2ServerlessPythonStack(app, "cdk-first-cdkv2-serverless-python")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
